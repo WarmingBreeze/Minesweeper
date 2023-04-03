e7d5de03ede9eb438671c396c78795ce35dbc8df
黃謝鈞 <huangxiejun@huangxiejuns-MacBook-Air.local>
2023-04-03 19:17:39 +0800

    work on Cell component

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react';
 import './Cell.css';
 import redFlagImage from '../images/red-flag.png';
+import explosion from '../images/nuclear-explosion.png';
 
 export default function Cell({id, value}){
-    const [isMarked, setIsMarked] = useState(false);
+    const [rightClicked, setRightClicked] = useState(false);
+    const [leftClicked, setLeftClicked] = useState(false);
+    const [displayImage, setDisplayImage] = useState(null);
     
     function handleRightClick(e){
         e.preventDefault();
-        setIsMarked(!isMarked);
+        if (!leftClicked){
+            setRightClicked(!rightClicked);
+            if (rightClicked) {
+                setDisplayImage({
+                    backgroundImage: `url(${redFlagImage})`,
+                    backgroundSize: `100% 100%`    
+                })
+            }
+        }
     }
     function handleClick(){
+        if (!rightClicked){
+            if (value === 'm'){
+                setDisplayImage(explosion);
+            } else if (value === 'b'){
 
+            } else {
+
+            }
+        }
     }
 
     return (
@@ -19,10 +38,11 @@ export default function Cell({id, value}){
             onContextMenu={handleRightClick}
             onClick={handleClick}
             style={
-                {backgroundImage: isMarked? `url(${redFlagImage})` : `none`,
-                backgroundSize: isMarked? `100% 100%`: `none`
+                {backgroundImage: rightClicked? `url(${redFlagImage})` : `none`,
+                backgroundSize: rightClicked? `100% 100%`: `none`
             }}
         >
+            {displayImage}
         </div>
     )
 }
