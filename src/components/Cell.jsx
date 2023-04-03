7b99ac3403168cd56148bf4ad409332a55c4f6f7
Shawn <xxx@express.com>
2023-04-03 19:58:31 +0800

    minor adjustment

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -4,13 +4,20 @@ import redFlagImage from '../images/red-flag.png';
 
 export default function Cell({id, value}){
     const [isMarked, setIsMarked] = useState(false);
-    
+    const [displayValue, setDisplayValue] = useState(null);
+
     function handleRightClick(e){
         e.preventDefault();
         setIsMarked(!isMarked);
     }
     function handleClick(){
-
+        if (value === 'm'){
+            setDisplayValue(<h6>Boom!</h6>);
+        } else if (value === 'b') {
+            setDisplayValue(<h6>blank</h6>);
+        } else {
+            setDisplayValue(<h6>{value}</h6>);
+        }
     }
 
     return (
@@ -23,6 +30,7 @@ export default function Cell({id, value}){
                 backgroundSize: isMarked? `100% 100%`: `none`
             }}
         >
+            {displayValue}
         </div>
     )
 }
